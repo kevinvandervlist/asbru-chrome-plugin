5901eea34662581ea37158b68c854b58b69c344d
Kevin van der Vlist <kevin@kevinvandervlist.nl>
2013-05-14 09:18:18 +0200

    Separate util 'package'

--- compile.sh
+++ compile.sh
@@ -6,15 +6,18 @@ if [ -d ${DEST_DIR} ]; then
 		rm -r ${DEST_DIR}
 fi
 
+# Utilities should always be included
+UTIL="${SRC_DIR}/scripts/util/"
+
 # Prepare output env
 cp -r ${SRC_DIR} ${DEST_DIR}
 find ${DEST_DIR} -type f -name "*.coffee" -exec rm -f {} \;
 
 # Compile plugin
-coffee -j "${DEST_DIR}/scripts/background.js" -c "${SRC_DIR}/scripts/background/"
-coffee -j "${DEST_DIR}/scripts/views.js" -c "${SRC_DIR}/scripts/views/"
+coffee -j "${DEST_DIR}/scripts/background.js" -c "${SRC_DIR}/scripts/background/" "${UTIL}"
+coffee -j "${DEST_DIR}/scripts/views.js" -c "${SRC_DIR}/scripts/views/" "${UTIL}"
 
 # Setup test env
-coffee -j "${DEST_DIR}/scripts/background.js" -c "${SRC_DIR}/tests/background/" "${SRC_DIR}/scripts/background/"
+coffee -j "${DEST_DIR}/scripts/background.js" -c "${SRC_DIR}/tests/background/" "${SRC_DIR}/scripts/background/" "${UTIL}"
 
-coffee -j "${DEST_DIR}/tests/views.tests.js" -c "${SRC_DIR}/tests/views/" "${SRC_DIR}/scripts/views/"
+coffee -j "${DEST_DIR}/tests/views.tests.js" -c "${SRC_DIR}/tests/views/" "${SRC_DIR}/scripts/views/" "${UTIL}"
---
+++ src/scripts/util/CircularBuffer.coffee
@@ -0,0 +1,44 @@
+class CircularBuffer
+  constructor: (req_size) ->
+    @size = req_size + 1
+    @store = new Array @size
+    @start = 0
+    @end = 0
+
+  isFull: ->
+    (@end + 1 ) % @size is @start
+
+  isEmpty: ->
+    @end is @start
+
+  push: (data) ->
+    @store[@end] = data
+    @end = (@end + 1) % @size
+    if @end is @start # Full -- circle is round
+      @start = (@start + 1) % @size
+
+  pop: ->
+    throw "Empty circular buffer!" if @isEmpty()
+    tmp = @start
+    @start = (@start + 1) % @size
+    @store[tmp]
+
+  capacity: ->
+    @size - 1
+
+  count: ->
+    if @end < @start then @end + @start else @end - @start
+
+  peek: (index = 0) ->
+    throw "Circular Buffer index out of range! #{index}" unless @count() > 0 and index < @count()
+    @store[(@start + index) % @size]
+
+  forEach: (func, context) ->
+    func.call(context, @peek(index), index) for index in [0...@count()]
+
+  toArray: ->
+    f = (el, index) ->
+      x.push el
+    x = []
+    @forEach f, x
+    x
